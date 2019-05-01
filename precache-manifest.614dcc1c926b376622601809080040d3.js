11a517c71cff610de0649ee23e6abf7a8ebfd3d7
kdybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-05-01 17:11:42 -0500

    Updates

---
+++ precache-manifest.614dcc1c926b376622601809080040d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aa5e7a950d27f7f5861",
+    "url": "/HooksDemo/static/js/runtime~main.73393fef.js"
+  },
+  {
+    "revision": "cae90ac167cb37a0177a",
+    "url": "/HooksDemo/static/js/main.c5366297.chunk.js"
+  },
+  {
+    "revision": "293ebf37227b08bc96c5",
+    "url": "/HooksDemo/static/js/2.fde2e10c.chunk.js"
+  },
+  {
+    "revision": "cae90ac167cb37a0177a",
+    "url": "/HooksDemo/static/css/main.eafdda7d.chunk.css"
+  },
+  {
+    "revision": "693e617b92296596ffeab94a51b740cc",
+    "url": "/HooksDemo/index.html"
+  }
+];
